771a54c067942015bd4dfb5f81ddac1d63470f74
PapaJool <81139787+PapaJool@users.noreply.github.com>
2023-12-26 23:52:55 +0300

    Update generate.py

--- code/generate.py
+++ code/generate.py
@@ -1,14 +1,28 @@
+import os
 import torch
 from model import RNNAttentionLanguageModel, Attention
 from utils import decode
 
-# Предполагаем, что у вас есть экземпляр модели 'model'
-# и стартовая последовательность 'start_tokens' (например, начальные символы из вашего текста)
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
+# Получаем путь к текущей директории скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Загрузка модели
+model = RNNAttentionLanguageModel()
+model.load_state_dict(torch.load(os.path.join(current_dir, 'checkpoints', 'your_model.pth')))
+model.eval()
+
+# Получаем путь к файлу input.txt в папке data
+input_file_path = os.path.join(current_dir, 'data', 'input.txt')
+
+# Загрузка стартовой последовательности из файла input.txt
+with open(input_file_path, 'r', encoding='utf-8') as f:
+    start_tokens = torch.tensor([int(token) for token in f.read().split()], dtype=torch.long).unsqueeze(0)
 
 # Генерация дополнительных токенов
 max_new_tokens = 500  # Задайте желаемое количество генерируемых токенов
-generated_tokens = model.generate(context, max_new_tokens, temperature=0.6)
+temperature = 0.6  # Задайте желаемую температуру
+
+generated_tokens = model.generate(start_tokens, max_new_tokens, temperature)
 
 # Декодирование сгенерированной последовательности в текст
 generated_text = decode(generated_tokens[0].tolist())
